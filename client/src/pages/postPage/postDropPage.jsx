68c187a78772971b2a0940a84ebaa97e42fe152d
darshank <darshankadam8@gmail.com>
2020-06-17 13:16:31 +0530

    dropzone added and css done

--- client/src/pages/postPage/postDropPage.jsx
+++ client/src/pages/postPage/postDropPage.jsx
@@ -1,12 +1,53 @@
 import React from 'react'
 import './postDropPage.scss'
+import Dropzone from 'react-dropzone'
+import {extractImageFileExtensionFromBase64} from '../../utils/functions/imageUtils'
+
+
+const imageMaxSize = 1000000000 // bytes
+const acceptedFileTypesArray = ['image/x-png', 'image/png', 'image/jpg', 'image/jpeg']
+const acceptedFileTypes = 'image/x-png, image/png, image/jpg, image/jpeg, image/gif'
+
 
 class PostDropPage extends React.Component{
+
+    handleOnDrop = (files) => {
+
+        if (files && files.length > 0){
+             const isVerified = this.verifyFile(files)
+             if (isVerified){
+                 // imageBase64Data 
+                 const currentFile = files[0]
+                 const myFileItemReader = new FileReader()
+                 myFileItemReader.addEventListener("load", ()=>{
+                     const myResult = myFileItemReader.result
+                     this.setState({
+                         imgSrc: myResult,
+                         extension: extractImageFileExtensionFromBase64(myResult)
+                     })
+                 }, false)
+                 myFileItemReader.readAsDataURL(currentFile)
+             }
+        }
+    }
     render(){
         return(
-            <React.Fragment>
-                {/* <div id="bg"></div> */}
-            </React.Fragment>
+            <main id="dropZone">
+                <Dropzone onDrop={this.handleOnDrop} accept={acceptedFileTypes} multiple={false}>
+                    {({getRootProps, getInputProps}) => (
+                        <section className="dropzone-container">
+                            <div {...getRootProps({className: 'dropzone'})}>
+                                <input {...getInputProps()} />
+                                <span id="a">Drop Image</span>
+                                <span id="b">To</span>
+                                <span id="c">Upload</span>
+
+
+                            </div>
+                        </section>
+                        )}
+                </Dropzone>
+            </main>
         )
     }
 }
